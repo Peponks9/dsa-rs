f71d1c9c6c9bd8d1287d818880e559fa14fd4e0c
Peponks9 <pepev821@gmail.com>
2025-09-18 09:55:15 -0600

    add insert from tail and index functions to singly-linked list

--- src/linked_lists.rs
+++ src/linked_lists.rs
@@ -33,6 +33,47 @@ impl<T> LinkedList<T> {
         self.head = Some(new_node);
         self.length += 1;
     }
+    // insert at tail
+    pub fn insert_at_tail(&mut self, value: T) {
+        let new_node = Box::new(Node { value, next: None });
+        if let Some(mut current) = self.head.as_mut() {
+            while let Some(ref mut next) = current.next {
+                current = next;
+            }
+            current.next = Some(new_node);
+        } else {
+            self.head = Some(new_node);
+        }
+        self.length += 1;
+    }
+    // insert at index
+    pub fn insert_at_index(&mut self, index: usize, value: T) -> Result<(), &'static str> {
+        if index > self.length {
+            return Err("Index out of bounds");
+        }
+        if index == 0 {
+            self.insert_at_head(value);
+            return Ok(());
+        }
+        let mut current = self.head.as_mut();
+        for _ in 0..index - 1 {
+            if let Some(node) = current {
+                current = node.next.as_mut();
+            }
+        }
+        if let Some(node) = current {
+            let new_node = Box::new(Node {
+                value,
+                next: node.next.take(),
+            });
+            node.next = Some(new_node);
+            self.length += 1;
+            Ok(())
+        } else {
+            Err("Index out of bounds")
+        }
+    }
+    
     // Remove from head
     pub fn remove_from_head(&mut self) -> Option<T> {
         if let Some(node) = self.head.take() {
@@ -43,5 +84,5 @@ impl<T> LinkedList<T> {
             None
         }
     }
-    // insert at tail
+    
 }
